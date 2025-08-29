15a5ed1d5cf6e16c583db09683a2f7d21d8246c2
draudrichman <88120992+draudrichman@users.noreply.github.com>
2025-08-30 02:14:00 +0600

    about sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import About from "./components/About";
 import Hero from "./components/Hero";
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
-      <section id="about" className="z-0 min-h-screen bg-black" />
+      <About />
     </main>
   );
 }
